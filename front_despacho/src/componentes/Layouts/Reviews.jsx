9680275c199aa5d4a9ca558af21418677a2a7e84
unknown <matias.chavez711@gmail.com>
2026-05-21 16:44:48 -0400

    cambios visuales

--- front_despacho/src/componentes/Layouts/Reviews.jsx
+++ front_despacho/src/componentes/Layouts/Reviews.jsx
@@ -8,7 +8,7 @@ function Reviews() {
         <div className="mx-auto mt-10 grid max-w-lg grid-cols-4 items-center gap-x-8 gap-y-10 sm:max-w-xl sm:grid-cols-6 sm:gap-x-10 lg:mx-0 lg:max-w-none lg:grid-cols-3">
           <img
             className="col-span-2 max-h-12 w-full object-contain lg:col-span-1 mx-auto"
-            src="https://upload.wikimedia.org/wikipedia/commons/f/fa/Apple_logo_black.svg"
+            src="https://www.asus.com/media/Odin/Websites/US/News/sjvqt8fhzfcjxkfg/tuf_logo.png"
             alt="Transistor"
             width="158"
             height="48"
